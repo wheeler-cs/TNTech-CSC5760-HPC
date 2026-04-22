ca69f1011a3fa96dee8b88eda854c80bbe458b50
Anna <wheelercs@protonmail.com>
2026-04-22 10:26:27 -0500

    Completed Problem 0
    Can convert from a local index to a global index using the linear mapping and from a global index to a local index using scatter mapping.
    
    Signed-off-by: Anna <wheelercs@protonmail.com>

--- Homework3/problem0.c
+++ Homework3/problem0.c
@@ -1,24 +1,25 @@
+/**
+ * Imagine that you have a vector v, of length M. You have P processes, and the
+ * vector is distributed among the processes twice—first as a linear mapping,
+ * then as a scatter mapping. This exercise is about global-to-local and
+ * local-to-global mappings in either linear or scattered distributions.
+ * 
+ * NOTE: In later problems, you will have such vectors distributed and
+ * replicated on process topologies of shape P × Q, but here we are just
+ * recalling how to map between local and global indices and processes in both
+ * mappings
+ */
+
 #include <stdio.h>
 
 #define P  4    //< Number of processes
 #define M 10    //< Length of vector
 
-int globalToLocalLinear(int rank, int globalIdx)
-{
-    // Function variables
-    int baseStride,
-        bonusElements,
-        localIdx;
-    baseStride = M / P;
-    bonusElements = M % P;
-
-    if(rank < bonusElements)
-    {
-        return 0;
-    }
-    return 0;
-}
-
+/**
+ * You have P processes, a vector v of length M, and you have the process rank
+ * value as p, with index in that process i. Find the global index I of that
+ * triple (p, i, M) with a linear inverse distribution.
+ */
 int localToGlobalLinear(int rank, int procIdx)
 {
     // Function variables
@@ -38,25 +39,26 @@ int localToGlobalLinear(int rank, int procIdx)
     // Rank has smaller stride
     else
     {
-        // Count all previous strides with extra elements
+        // Calculate offset using mix of longer and shorter stide lengths
         globalIdx = ((baseStride + 1) * bonusElements);
-        // Count all previous stides with no extra elements
-        globalIdx += ((baseStride * (rank - bonusElements)));
-        // Count offset of local process index
-        globalIdx += procIdx;
+        globalIdx += ((baseStride * (rank - bonusElements))) + procIdx;
     }
 
     return globalIdx;
 }
 
-int globalToLocalScatter(int rank, int globalIdx)
+/**
+ * Given that same global index I, the length of the array M, and the number of
+ * processes P, is it possible to find the rank of the process p, and the local
+ * index i of its scatter-mapped index? How?
+ */
+int globalToLocalScatter(int globalIdx, int * rank)
 {
-    return 0;
-}
+    // Find owning rank for index
+    *rank = globalIdx % P;
 
-int localToGlobalScatter(int rank, int procIdx)
-{
-    return 0;
+    // Calculate local index within rank
+    return (globalIdx / P);
 }
 
 int main(int argc, char ** argv)
@@ -64,6 +66,7 @@ int main(int argc, char ** argv)
     int i, j;
 
     // Do local to global for linear distribution
+    printf("Local-to-global conversion for linear distribution\n");
     for(i = 0; i < P; i++)
     {
         printf("Rank %d: ", i);
@@ -77,6 +80,16 @@ int main(int argc, char ** argv)
         {
             printf("%d\n", localToGlobalLinear(i, 1));
         }
+    }
+    printf("\n");
+
+    // Do global to local for scatter distribution
+    int rank, localIdx;
+    printf("Global-to-local conversion for scatter distribution\n");
+    for(i = 0; i < M; i++)
+    {
+        localIdx = globalToLocalScatter(i, &rank);
+        printf("Rank: %d, Idx: %d\n", rank, localIdx);
     }
 
     return 0;
